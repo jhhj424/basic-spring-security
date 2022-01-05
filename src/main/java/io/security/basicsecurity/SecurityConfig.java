f1c41a22134f1de02d156bf61be15b772419a0f5
zito-s <jhhj424@naver.com>
2022-01-05 23:37:35 +0900

    feat: Form LoginOut 인증 설정

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -5,6 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Slf4j
 @Configuration
@@ -17,22 +25,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated();
         http
-                .formLogin()
+                .formLogin();
 //                .loginPage("/loginPage")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login")
-                .usernameParameter("userId")
-                .passwordParameter("passwd")
-                .loginProcessingUrl("/login_proc")
-                .successHandler((request, response, authentication) -> {
-                    log.info("authentication: {}", authentication.getName());
-                    response.sendRedirect("/");
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/login")
+//                .usernameParameter("userId")
+//                .passwordParameter("passwd")
+//                .loginProcessingUrl("/login_proc")
+//                .successHandler((request, response, authentication) -> {
+//                    log.info("authentication: {}", authentication.getName());
+//                    response.sendRedirect("/");
+//                })
+//                .failureHandler((request, response, exception) -> {
+//                    log.error("exception: {}", exception.getMessage());
+//                    response.sendRedirect("/login");
+//                })
+//                .permitAll()
+        ;
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler((request, response, authentication) -> {
+                    request.getSession().invalidate();
                 })
-                .failureHandler((request, response, exception) -> {
-                    log.error("exception: {}", exception.getMessage());
+                .logoutSuccessHandler((request, response, authentication) -> {
                     response.sendRedirect("/login");
                 })
-                .permitAll()
-        ;
+                .deleteCookies("remember-me")
+                ;
     }
 }
