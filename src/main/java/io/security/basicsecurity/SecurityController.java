f088c8817cf8c1c4a12344d039ea8f6785c666af
zito-s <jhhj424@naver.com>
2022-01-24 22:50:07 +0900

    feat: add security exceptionHandling

--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SecurityController {
 
-    @GetMapping("loginPage")
-    public String loginPage() {
-        return "loginPage";
-    }
-
     @GetMapping("/")
     public String index() {
         return "home";
@@ -29,5 +24,15 @@ public class SecurityController {
     @GetMapping("/admin/**")
     public String admin() {
         return "admin";
+    }
+
+    @GetMapping("/denied")
+    public String denied() {
+        return "Access is denied";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
