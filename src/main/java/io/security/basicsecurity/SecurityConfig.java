e40c961348a636052002924604c533fadd3ee841
zito-s <jhhj424@naver.com>
2022-02-04 15:19:53 +0900

    feat: SecurityConfig

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -81,54 +81,54 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .disable()
         ;
 
-//        http
-//                .authorizeRequests()
-//                .anyRequest().authenticated();
-//        http
-//                .formLogin()
-////                .loginPage("/loginPage")
-//                .defaultSuccessUrl("/")
-//                .failureUrl("/login")
-//                .usernameParameter("userId")
-//                .passwordParameter("passwd")
-//                .loginProcessingUrl("/login_proc")
-//                .successHandler((request, response, authentication) -> {
-//                    log.info("authentication: {}", authentication.getName());
-//                    response.sendRedirect("/");
-//                })
-//                .failureHandler((request, response, exception) -> {
-//                    log.error("exception: {}", exception.getMessage());
-//                    response.sendRedirect("/login");
-//                })
-//                .permitAll()
-//        ;
-//        http
-//                .logout()
-//                .logoutUrl("/logout")
-//                .logoutSuccessUrl("/login")
-//                .addLogoutHandler((request, response, authentication) -> {
-//                    request.getSession().invalidate();
-//                })
-//                .logoutSuccessHandler((request, response, authentication) -> {
-//                    response.sendRedirect("/login");
-//                })
-//        ;
-//        http
-//                .rememberMe()
-//                .rememberMeParameter("remember")
-//                .tokenValiditySeconds(3600)
-//                .userDetailsService(userDetailsService)
-//        ;
-//        http
-//                .sessionManagement()
-//                .maximumSessions(1)
-//                .maxSessionsPreventsLogin(true)
-////                .expiredUrl("/expired")
-//        ;
-////        http
-////                .sessionManagement()
-////                .sessionFixation()
-////                .changeSessionId() // 기본적으로 스프링 시큐리티가 설정해줌
-////        ;
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+        http
+                .formLogin()
+//                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler((request, response, authentication) -> {
+                    log.info("authentication: {}", authentication.getName());
+                    response.sendRedirect("/");
+                })
+                .failureHandler((request, response, exception) -> {
+                    log.error("exception: {}", exception.getMessage());
+                    response.sendRedirect("/login");
+                })
+                .permitAll()
+        ;
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler((request, response, authentication) -> {
+                    request.getSession().invalidate();
+                })
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    response.sendRedirect("/login");
+                })
+        ;
+        http
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService)
+        ;
+        http
+                .sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true)
+                .expiredUrl("/expired")
+        ;
+        http
+                .sessionManagement()
+                .sessionFixation()
+                .changeSessionId() // 기본적으로 스프링 시큐리티가 설정해줌
+        ;
     }
 }
